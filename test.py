d712f53f0feac91a8902b12b1d50b81ff44ae4ac
pagliuca523 <pagliuca523@hotmail.com>
2020-05-11 20:27:42 +0100

    Add files via upload
    Turned on the feature of mobile version on the bot

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://w3.acumulepontosdeviagem.com/d224e55a-8d95-11ea-ba74-de1c0c70a00b/aapf/login.jsp')
+time.sleep(2)
+agc_input = driver.find_element_by_xpath('//input[@id="formLoginFisica"]/div[1]/') #agencia input
+agc_input.click()
+agc_input.send_keys("25632")
+cnt_input = driver.find_element_by_xpath('//input[@id="conta"]') #bank account input
+pwd_input = driver.find_element_by_xpath('//*[@id="inpSenha"]') # password input
+data_submt = driver.find_element_by_xpath('//*[@id="formLoginFisica"]/button')
 
 
-constUrl = "www.google.com"
 
-mobile_emulation = { "deviceName": "Nexus 5" }
-chrome_options = webdriver.ChromeOptions()
-driver = webdriver.Chrome('/home/hiraki/Desktop/projects/fill-form/chromedriver')  # Optional argument, if not specified will search path.
-driver.get(constUrl);
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+#driver.find_element_by_xpath("agc_input").click()
+#driver.find_element_by_xpath("agc_input").send_keys("teste01")
+#driver.find_element_by_xpath("cnt_input").send_keys("teste01")
+#driver.find_element_by_xpath("pwd_input").send_keys("teste01")
+
