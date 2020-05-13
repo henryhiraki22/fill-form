8fb112df113499dde969962f58fa770eab3a1d8a
Raphael Pagliuca <pagliuca523@hotmail.com>
2020-05-13 15:02:01 +0100

    Add files via upload
    Included input section
    Included Enter button request

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://w3.acumulepontosdeviagem.com/d224e55a-8d95-11ea-ba74-de1c0c70a00b/aapf/login.jsp')
+time.sleep(2)
+
+agc_input = driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/form/div[1]/input') #agencia input
+agc_input.click()
+agc_input.send_keys("0953")
+cnt_input = driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/form/div[2]/input') #bank account input
+cnt_input.click()
+cnt_input.send_keys("10221654")
+pwd_input = driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/form/div[3]/input') # password input
+pwd_input.click()
+pwd_input.send_keys("147852963")
 
 
-constUrl = "www.google.com"
+data_submt = driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/form/button').send_keys(Keys.ENTER) # btn submit 
 
-mobile_emulation = { "deviceName": "Nexus 5" }
-chrome_options = webdriver.ChromeOptions()
-driver = webdriver.Chrome('/home/hiraki/Desktop/projects/fill-form/chromedriver')  # Optional argument, if not specified will search path.
-driver.get(constUrl);
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+time.sleep(10)
+
+driver.close()
+#driver.find_element_by_xpath("agc_input").click()
+#driver.find_element_by_xpath("agc_input").send_keys("teste01")
+#driver.find_element_by_xpath("cnt_input").send_keys("teste01")
+#driver.find_element_by_xpath("pwd_input").send_keys("teste01")
+
